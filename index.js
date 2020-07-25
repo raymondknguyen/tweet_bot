b8389c179c295a5c2409a3a6eb2fb4048b5627f6
Taras Tarlov <ttarlov@gmail.com>
2020-07-24 20:23:16 -0600

    Add new instance of nightmare in the main function

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
 const Nightmare = require('nightmare');
-const nightmare = Nightmare({ show: true });
+let nightmare = Nightmare({ show: true });
 //run npm start or nodemon (if you have nodemon installed globally)
 // to run the app locally
 //this sets the post at 3001 - go to localhost:3001 in your browser to see app
 app.set('port', process.env.PORT || 3001);
 app.locals.title = 'Twitter Hackery';
-let manualRides = []
 // at localhost:3001/, you should see the text in this response
 
 app.get('/send-tweet', async (request, response) => {
@@ -20,6 +19,7 @@ app.get('/send-tweet', async (request, response) => {
     console.log(err);
   }
 });
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
 });
@@ -40,5 +40,13 @@ function sendTweet(message, handle) {
       let tweetBtn = [...document.querySelectorAll('span')].find(span => span.innerText === "Tweet")
       tweetBtn.addEventListener("click", e => { console.log("clicked") })
       tweetBtn.click()
+    })
+    .wait(2000)
+    .then(() => {
+      return nightmare.end();
+    })
+    .then(function(){
+      nightmare  = Nightmare()
+      console.log("nightmare done")
     })
 }
